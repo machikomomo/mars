01b420dbed28d6224c04828e20602cc2e060c32b
momochan <momochan@example.com>
2021-03-06 22:43:38 +0800

    add delete

--- src/main/java/com/momoka/marsdemo/MarsDemoApplication.java
+++ src/main/java/com/momoka/marsdemo/MarsDemoApplication.java
@@ -1,9 +1,11 @@
 package com.momoka.marsdemo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.momoka.marsdemo.dao")
 public class MarsDemoApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/momoka/marsdemo/controller/UserController.java
+++ src/main/java/com/momoka/marsdemo/controller/UserController.java
@@ -1,20 +1,28 @@
 package com.momoka.marsdemo.controller;
 
 import com.momoka.marsdemo.dao.User;
+import com.momoka.marsdemo.dao.UserDao;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.annotation.Resource;
+
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+    @Resource
+    private UserDao userDao;
+
     @RequestMapping("")
-    public ModelAndView home(@RequestParam String name, Integer num) {
-        User user = new User();
-        user.setName("akira");
-        user.setPasswd("123456");
+    public ModelAndView home(Long id,Long idDel) {
+        User user = userDao.selectByPrimaryKey(id);
+        userDao.deleteByPrimaryKey(idDel);
+//        User user = new User();
+//        user.setName("akira");
+//        user.setPasswd("123456");
 
         ModelAndView modelMap = new ModelAndView();
         modelMap.addObject("user", user);
